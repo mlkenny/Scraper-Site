16107fb2afa09de6404bc8f6fa2f32c5d70e2a3f
mlkenny <MichaelK1734@gmail.com>
2025-11-15 16:19:33 -0800

    Added character model to models.py and allowed scraper manager to save character model to db. Opted to not save quotes as to save space in DB, may need to be changed to avoid saving quotes to hard disk.

--- scraper/models.py
+++ scraper/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class Character(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    dataset_path = models.FilePathField(path="datasets/", null=True, blank=True)
+    dataset_path = models.FilePathField(path="scraper/datasets/", null=True, blank=True)
     model_id = models.CharField(max_length=255, null=True, blank=True)  # fine-tuned model ID
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
